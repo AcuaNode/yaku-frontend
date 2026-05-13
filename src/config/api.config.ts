101c0c1398cc3ecdc14ba98ccce1d9045de9078b
Mario <mariojoaquin06@hotmail.com>
2026-05-13 03:45:05 -0500

    feat(develop): integre

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080/api'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://yaku-back-b5ggakd7awhucvaq.canadacentral-01.azurewebsites.net/api/v1'
 
 export const API_ENDPOINTS = {
   auth: {
